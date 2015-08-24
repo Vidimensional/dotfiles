de6d23b501c46a33322d12420f4a6329a01d0ec2
Daniel Vidal <daniel@typeform.com>
2015-08-24 15:53:18 +0200

    Separate installation steps into functions.

--- install.sh
+++ install.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
+log () {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] $@"
+}
+
+
+install_bashconfig () {
+    log "Installing .bashrc"
+    rsync -rvc .bashrc* ~/
+
+    log "Downloading & installing git-prompt"
+    git_prompt_url='https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh'
+    curl -s "${git_prompt_url}" -o ~/.bashrc.d/bash.git-prompt
+}
+
+install_vimconfig () {
+    log "Installing .vimrc"
+    rsync -rvc .vim* ~/
+}
+
 case $1 in
     'bash')
-	rsync -rvc .bashrc* ~/
+        install_bashconfig
         ;;
 
     'vim')
-        rsync -rvc .vim* ~/
+        install_vimconfig
 	;;
+
+    'all')
+        install_bashconfig
+        install_vimconfig
+	;;
+
+    *)
+        echo "USAGE $0 <bash|vim>"
+        ;;
 esac
 
