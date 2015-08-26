8b6ee5a111f60392b58fafbc9253b01ac5670a6a
Daniel Vidal <daniel@typeform.com>
2015-08-26 12:52:51 +0200

    Install script with colors. :)

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+source .bashrc.d/bash.colors
+
+
 log () {
+    echo -ne "${__YELLOW}"
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] $@"
+    echo -ne "${__DEFAULT}"
 }
 
 
@@ -14,6 +19,7 @@ install_bashconfig () {
     curl -s "${git_prompt_url}" -o ~/.bashrc.d/bash.git-prompt
 }
 
+
 install_vimconfig () {
     log "Installing vimrc."
     rsync -rvc .vim* ~/
@@ -21,30 +27,28 @@ install_vimconfig () {
     vim +PluginInstall +qall
 }
 
+
 install_i3config () {
     log "Installing i3 config."
     rsync -rvc .i3 ~/
 }
 
+
 case $1 in
     'bash')
         install_bashconfig
         ;;
-
     'vim')
         install_vimconfig
 	;;
-
     'i3')
         install_i3config
 	;;
-
     'all')
         install_bashconfig
         install_vimconfig
         install_i3config
 	;;
-
     *)
         echo "USAGE $0 <bash|vim|i3>"
         ;;
