07af46f7e536ae37aa1d69dd0bc6c8ea982b4230
Daniel Vidal <daniel@typeform.com>
2017-01-25 14:27:10 +0100

    Change directory structure.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source bashrc.d/bash.colors
+source bash/bashrc.d/colors.bash
 
 
 log () {
@@ -16,23 +16,23 @@ _rsync () {
 
 install_bashconfig () {
     log "Installing bashrc."
-    cp -a bashrc ~/.bashrc
-    _rsync bashrc.d/* ~/.bashrc.d
+    cp -v bash/bashrc ~/.bashrc
+    _rsync bash/bashrc.d/* ~/.bashrc.d
     log "Downloading & installing git-prompt"
     git_prompt_url='https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh'
-    curl -s "${git_prompt_url}" -o ~/.bashrc.d/bash.git-prompt
+    curl -s "${git_prompt_url}" -o ~/.bashrc.d/git-prompt.bash
     log "Downloading & installing bash completion for Hub."
     hub_autocompletion_url='https://raw.githubusercontent.com/github/hub/master/etc/hub.bash_completion.sh'
-    curl -s "${hub_autocompletion_url}" -o  ~/.bashrc.d/bash.hub.bash_completion.sh
-    reloadbash #Alias that resources the .bashrc.
+    curl -s "${hub_autocompletion_url}" -o  ~/.bashrc.d/hub.bash_completion.bash
+    source ~/.bashrc
     log "Done."
 }
 
 
 install_vimconfig () {
     log "Installing vimrc."
-    cp vimrc ~/.vimrc
-    _rsync vimrc.d/* ~/.vimrc.d
+    cp -v vim/vimrc ~/.vimrc
+    _rsync vim/vimrc.d/* ~/.vimrc.d
     log "Installing Vim plugins via Plug."
     vim +PlugInstall +qall
     log "Done."
