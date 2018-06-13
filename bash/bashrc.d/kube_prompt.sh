a9dc690730a8dd76d9f37ad2c6cdb1422b98c7d1
Daniel Vidal de la Rubia <dvrubia@gmail.com>
2018-06-13 09:29:19 +0200

    Add attribution of the idea of kubepromt.

--- bash/bashrc.d/kube_prompt.sh
+++ bash/bashrc.d/kube_prompt.sh
@@ -1,3 +1,7 @@
+# This trick is inspired on https://github.com/jonmosco/kube-ps1
+# I had problems with kube-ps1 and character encoding on my terminal. And it was
+# faster to adapt a simpler version of the script.
+
 kpon() { echo 'true' > /tmp/kubeprompt; }
 kpoff() { echo 'false' > /tmp/kubeprompt; }
 
