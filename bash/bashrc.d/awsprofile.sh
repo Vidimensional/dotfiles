ff1073c25bb07e54324e22a7001cc31b8786c616
Vidal <dvrubia@gmail.com>
2018-10-12 12:44:57 +0200

    Add awsprofile function.

--- bash/bashrc.d/awsprofile.sh
+++ bash/bashrc.d/awsprofile.sh
@@ -5,3 +5,35 @@ _awsprompt() {
   
   echo "[AWS->${aws_profile}]"
 }
+
+_aws_list_profiles() {
+    grep '\[' ~/.aws/credentials | tr -d ']['
+}
+
+awsprofile() {
+    local profiles changed
+    
+    profiles="$(_aws_list_profiles)"
+    changed=0
+    
+    if [[ $# -eq 0 ]]; then
+        echo $profiles | tr ' ' '\n'
+        return 0
+    fi
+
+    for profile in $profiles; do
+        if [[ "$1" == "$profile" ]]; then
+            export AWS_PROFILE=$1
+            changed=1
+            break
+        fi
+    done
+
+    if [[ "$changed" -eq 1 ]]; then
+        echo "AWS profile changed to $1."
+        return 0
+    else
+        echo "Profile $1 doesn't exist."
+        return 1
+    fi
+}
