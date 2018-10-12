087193e35a2618832918bfac8f9e193c3353eba6
Daniel Vidal de la Rubia <daniel@typeform.com>
2018-10-12 12:48:28 +0200

    Aws profile manager (#4)
    * Rename file.
    
    * Add awsprofile function.

--- bash/bashrc.d/aws_prompt.sh
+++
@@ -1,7 +0,0 @@
-_awsprompt() {
-  local aws_profile
-  aws_profile="$(env | grep AWS_PROFILE | cut -d= -f2)"
-  [[ "${aws_profile}" == "" ]] && aws_profile=default
-  
-  echo "[AWS->${aws_profile}]"
-}
