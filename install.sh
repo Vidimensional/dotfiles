b5d684b7acf9b3c179165673490393ed567cc079
Daniel Vidal de la Rubia <dvrubia@gmail.com>
2019-11-04 14:20:39 +0100

    gitignore JetBrains folder

--- install.sh
+++ install.sh
@@ -59,7 +59,6 @@ install_vimconfig () {
 install_gitconfig () {
     log "Installing gitconfig."
     cp -v git/gitconfig ~/.gitconfig
-    cp -v git/gitignore_global ~/.gitignore_global
     _rsync git/gitconfig.d/* ~/.gitconfig.d/
 }
 
