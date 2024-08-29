33a39525b25d68083980be772b28db58df44dc00
Daniel Vidal de la Rubia <dvrubia@gmail.com>
2024-08-29 16:43:44 +0200

    Install GNU tools on MacOS

---
+++ roles/gnutools/files/gnutools.bash
@@ -0,0 +1,8 @@
+# This file is meant to be sourced from bashrc
+
+PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:${PATH}"
+PATH="/opt/homebrew/opt/gawk/libexec/gnubin:${PATH}"
+PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:${PATH}"
+PATH="/opt/homebrew/opt/gnu-tar/libexec/gnubin:${PATH}"
+
+export PATH
