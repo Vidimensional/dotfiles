a6eae6ad3e4b569fcae00e05003b8533e61d5d02
Daniel Vidal de la Rubia <dvrubia@gmail.com>
2025-10-08 21:37:57 +0200

    Bash: Move GNUTools paths to its own role

--- roles/bash/files/bashrc.d/path.bash
+++ roles/bash/files/bashrc.d/path.bash
@@ -12,14 +12,6 @@ case `uname -s` in
         # export GOROOT="/usr/local/opt/go/libexec"
         # PATH="${PATH}:${GOPATH}/bin:${GOROOT}/bin"
 
-        # Add GNU coreutils manpages to $MANPATH.
-        export MANPATH=":$MANPATH:/usr/local/opt/coreutils/libexec/gnuman"
-
-        # Add GNU tools $PATH (override BSD ones).
-        PATH="${PATH}:/usr/local/opt/coreutils/libexec/gnubin"
-        PATH="${PATH}:/usr/local/opt/grep/libexec/gnubin"
-        PATH="${PATH}:/usr/local/opt/gnu-sed/libexec/gnubin"
-
         PATH="${PATH}:/usr/local/opt/php@7.4/bin"
         PATH="${PATH}:/usr/local/opt/mysql-client/bin"
         PATH="${PATH}:/usr/local/opt/libpq/bin"
