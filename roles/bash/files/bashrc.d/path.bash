ecca65339968f9c9b3b1a4a8c580137090fb0cdc
Daniel Vidal de la Rubia <dvrubia@gmail.com>
2025-10-08 21:41:42 +0200

    Bash: PHP paths shouldn't be defined anywhere

--- roles/bash/files/bashrc.d/path.bash
+++ roles/bash/files/bashrc.d/path.bash
@@ -6,12 +6,6 @@ case `uname -s` in
         PATH="${PATH}:${HOME}/.local/bin"
         PATH="${PATH}:/snap/bin"
         ;;
-    
-    Darwin)
-        PATH="${PATH}:/usr/local/opt/php@7.4/bin"
-        PATH="${PATH}:/usr/local/opt/mysql-client/bin"
-        PATH="${PATH}:/usr/local/opt/libpq/bin"
-        ;;
 esac
 
 export PATH
