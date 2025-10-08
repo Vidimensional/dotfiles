10a2d7f4c1f416daad6ca252ee3311c3d065e574
Daniel Vidal de la Rubia <dvrubia@gmail.com>
2025-10-08 21:28:55 +0200

    Bash: remove unused aliases

--- roles/bash/files/bashrc.d/aliases.bash
+++ roles/bash/files/bashrc.d/aliases.bash
@@ -12,36 +12,7 @@ alias ln='ln -v'
 
 repos_path=~/dev/vidi
 alias dotfiles="code ${repos_path}/dotfiles"
-
 alias reloadbash='source ~/.bashrc'
-
-alias unvim="find . -iname '.*.swp' -delete"
-
-alias tiempo="curl wttr.in/"
-
-alias mp="multipass"
-
-#Commands running on Docker containers.
-#alias psql='docker run -it -v "${PWD}:/opt" -e "PGPASSWORD=${PGPASSWORD}" vidimensional/psql'
-#alias mysql='docker run -it vidimensional/mysql-client'
-
-#alias kctl_proxy='kubectl proxy >/dev/null 2>&1 &'
-alias terminate_instance='aws ec2 terminate-instances --instance-ids'
-
-# Turns tabs into 4 spaces.
-alias untab='sed "s/\t/    /g"' 
-
-alias dkillall='docker ps --all --format={{.ID}} | xargs -- docker rm --force'
-
-certview(){
-    for cert in $@; do
-        openssl x509 -in "${cert}" -text -noout
-    done
-}
-
-secret() {
-    curl -s -u "${ONETIMESECRET_API_TOKEN}" -F "secret=$1" https://onetimesecret.com/api/v1/share | jq -r '"https://onetimesecret.com/secret/\(.secret_key)"'
-}
 
 __log(){
     echo -e "> ${__YELLOW}$@${__DEFAULT}"
