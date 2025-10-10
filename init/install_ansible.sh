06fb536b8e0bfab191d5b5503a72cb18e50678f8
Daniel Vidal de la Rubia <dvrubia@gmail.com>
2025-10-10 17:21:34 +0200

    Install molecule using an Ansible playbook

--- init/install_ansible.sh
+++ init/install_ansible.sh
@@ -4,16 +4,18 @@
 
 set -euo pipefail
 
+# Installing pipx depends on the OS
 case "$( uname -s )" in
   Linux)
     sudo apt-get update
-    sudo apt-get install --assume-yes pipx
-    pipx install --include-deps ansible
+    sudo apt-get install --assume-yes python3-pip
+    sudo pip install --break-system-packages pipx # We need at least version 1.7.0 (this should be fixed when upgrading to Ubuntu 25.04)
     ;;
   Darwin)
     /bin/bash -c "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh )"
-    brew install ansible
+    brew install pipx
     ;;
 esac
 
+pipx install --include-deps ansible
 ansible-galaxy install -r collections/requirements.yml
