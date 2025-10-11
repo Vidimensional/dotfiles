306d8d25de35c15cd6300ff64c49e4cafedd7a32
Daniel Vidal de la Rubia <dvrubia@gmail.com>
2025-10-11 21:49:46 +0200

    Revert "Update scripts/install-pipx.sh"
    This reverts commit ec8b3c4f1c70a5cdcfdd06bffe0267b02aaeede7.
    
    Wrong copilot suggestion

--- scripts/install-pipx.sh
+++ scripts/install-pipx.sh
@@ -8,7 +8,7 @@ case "$( uname -s )" in
   Linux)
     sudo apt-get update
     sudo apt-get install --assume-yes python3-pip python3-venv
-    pip install --user pipx # We need at least version 1.7.0 (this should be fixed when upgrading to Ubuntu 25.04)
+    sudo pip install --break-system-packages pipx # We need at least version 1.7.0 (this should be fixed when upgrading to Ubuntu 25.04)
     ;;
   Darwin)
     /bin/bash -c "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh )"
