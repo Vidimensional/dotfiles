172c6f415bd6bd074716818f5ee8584942a745c0
Daniel Vidal de la Rubia <dvrubia@gmail.com>
2025-10-11 21:34:36 +0200

    We also need venv for using pipx

--- scripts/install-pipx.sh
+++ scripts/install-pipx.sh
@@ -7,11 +7,11 @@ set -euo pipefail
 case "$( uname -s )" in
   Linux)
     sudo apt-get update
-    sudo apt-get install --assume-yes python3-pip
+    sudo apt-get install --assume-yes python3-pip python3-venv
     sudo pip install --break-system-packages pipx # We need at least version 1.7.0 (this should be fixed when upgrading to Ubuntu 25.04)
     ;;
   Darwin)
     /bin/bash -c "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh )"
-    brew install pipx
+    brew install pipx virtualenv
     ;;
 esac
